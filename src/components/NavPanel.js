c7f62d992b60f726a2b24daffe0a667104d17ff8
Giancarlo Trentini <giancarlo.trentini@gmail.com>
2021-12-14 11:51:00 -0500

    more nav styles and markup

--- src/components/NavPanel.js
+++ src/components/NavPanel.js
@@ -6,207 +6,340 @@ import TempIcon from './TempIcon';
 const NavPanel = ({ label, path, children }) => {
   if (children.length > 0) {
     return (
-      // <li className="nav-item dropdown">
-      //   <a
-      //     className="nav-link dropdown-toggle"
-      //     href={path}
-      //     data-bs-toggle="dropdown"
-      //   >
-      //     {label}
-      //   </a>
-      //   <ul className="dropdown-menu">
-      //     {children.map((item, index) => (
-      //       <li>
-      //         <Link
-      //           className="dropdown-item"
-      //           to={item.path}
-      //           target={item.target}
-      //         >
-      //           {item.label}
-      //         </Link>
-      //       </li>
-      //     ))}
-      //   </ul>
-      // </li>
-      <li className="nav-item dropdown" style={{ position: 'static' }}>
-        <a
-          className="nav-link dropdown-toggle"
-          href="#"
-          id="navbarDropdown"
-          role="button"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          Solutions
-        </a>
-        <ul
-          className="dropdown-menu"
-          aria-labelledby="navbarDropdown"
-          style={{ width: '1200px' }}
-        >
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-3">
-                <p className="dropdown-section-title">By Need</p>
-                <li>
-                  <Link className="dropdown-item" href="#">
-                    <TempIcon iconSize="12" />
-                    Commerce
-                    <br />
-                    <span className="dropdown-item-content">
-                      Brief description of commerce benefits across up to two
-                      lines
-                    </span>
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" href="#">
-                    <TempIcon iconSize="12" />
-                    Customer Care
-                    <br />
-                    <span className="dropdown-item-content">
-                      Brief description of commerce benefits across up to two
-                      lines
-                    </span>
-                  </Link>
-                </li>
-              </div>
-              <div className="col-lg-3">
-                <p className="dropdown-section-title">By Industry</p>
-                <li>
-                  <Link className="dropdown-item" href="#">
-                    <TempIcon iconSize="12" />
-                    Healthcare
-                    <br />
-                    <span className="dropdown-item-content">
-                      Brief description of commerce benefits across up to two
-                      lines
-                    </span>
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" href="#">
-                    <TempIcon iconSize="12" />
-                    Automotive
-                    <br />
-                    <span className="dropdown-item-content">
-                      Brief description of commerce benefits across up to two
-                      lines
-                    </span>
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" href="#">
-                    <TempIcon iconSize="12" />
-                    Customer Care
-                    <br />
-                    <span className="dropdown-item-content">
-                      Brief description of commerce benefits across up to two
-                      lines
-                    </span>
-                  </Link>
-                </li>
-              </div>
-              <div className="col-lg-3">
-                <p className="dropdown-section-title">&nbsp;</p>
-                <li>
-                  <Link className="dropdown-item" href="#">
-                    <TempIcon iconSize="12" />
-                    Financial Services
-                    <br />
-                    <span className="dropdown-item-content">
-                      Brief description of commerce benefits across up to two
-                      lines
-                    </span>
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" href="#">
-                    <TempIcon iconSize="12" />
-                    Retail
-                    <br />
-                    <span className="dropdown-item-content">
-                      Brief description of commerce benefits across up to two
-                      lines
-                    </span>
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" href="#">
-                    <TempIcon iconSize="12" />
-                    Travel/Hospitality
-                    <br />
-                    <span className="dropdown-item-content">
-                      Brief description of commerce benefits across up to two
-                      lines
-                    </span>
-                  </Link>
-                </li>
-              </div>
-              <div className="col-lg-3 dropdown-section-gray">
-                <p className="dropdown-section-title">Products</p>
-                <li>
-                  <Link className="dropdown-item" href="#">
-                    <TempIcon iconSize="56" />
-                    <div>
-                      Conversational Cloud
+      <>
+        <li className="nav-item dropdown" style={{ position: 'static' }}>
+          <a
+            className="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Solutions
+          </a>
+          <ul
+            className="dropdown-menu"
+            aria-labelledby="navbarDropdown"
+            style={{ width: '1200px' }}
+          >
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-3">
+                  <p className="dropdown-section-title">By Need</p>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      <TempIcon iconSize="12" />
+                      Commerce
                       <br />
                       <span className="dropdown-item-content">
                         Brief description of commerce benefits across up to two
                         lines
                       </span>
-                    </div>
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" href="#">
-                    <TempIcon iconSize="56" />
-                    <div>
-                      Voice
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      <TempIcon iconSize="12" />
+                      Customer Care
                       <br />
                       <span className="dropdown-item-content">
                         Brief description of commerce benefits across up to two
                         lines
                       </span>
-                    </div>
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" href="#">
-                    <TempIcon iconSize="56" />
-                    <div>
-                      Managed Services
+                    </Link>
+                  </li>
+                </div>
+                <div className="col-lg-3">
+                  <p className="dropdown-section-title">By Industry</p>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      <TempIcon iconSize="12" />
+                      Healthcare
                       <br />
                       <span className="dropdown-item-content">
                         Brief description of commerce benefits across up to two
                         lines
                       </span>
-                    </div>
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" href="#">
-                    <TempIcon iconSize="56" />
-                    <div>
-                      Bella Health
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      <TempIcon iconSize="12" />
+                      Automotive
                       <br />
                       <span className="dropdown-item-content">
                         Brief description of commerce benefits across up to two
                         lines
                       </span>
-                    </div>
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item dropdown-link-blue" href="#">
-                    See Pricing & Packages
-                  </Link>
-                </li>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      <TempIcon iconSize="12" />
+                      Customer Care
+                      <br />
+                      <span className="dropdown-item-content">
+                        Brief description of commerce benefits across up to two
+                        lines
+                      </span>
+                    </Link>
+                  </li>
+                </div>
+                <div className="col-lg-3">
+                  <p className="dropdown-section-title">&nbsp;</p>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      <TempIcon iconSize="12" />
+                      Financial Services
+                      <br />
+                      <span className="dropdown-item-content">
+                        Brief description of commerce benefits across up to two
+                        lines
+                      </span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      <TempIcon iconSize="12" />
+                      Retail
+                      <br />
+                      <span className="dropdown-item-content">
+                        Brief description of commerce benefits across up to two
+                        lines
+                      </span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      <TempIcon iconSize="12" />
+                      Travel/Hospitality
+                      <br />
+                      <span className="dropdown-item-content">
+                        Brief description of commerce benefits across up to two
+                        lines
+                      </span>
+                    </Link>
+                  </li>
+                </div>
+                <div className="col-lg-3 dropdown-section-gray">
+                  <p className="dropdown-section-title">Products</p>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      <TempIcon iconSize="56" />
+                      <div>
+                        Conversational Cloud
+                        <br />
+                        <span className="dropdown-item-content">
+                          Brief description of commerce benefits across up to
+                          two lines
+                        </span>
+                      </div>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      <TempIcon iconSize="56" />
+                      <div>
+                        Voice
+                        <br />
+                        <span className="dropdown-item-content">
+                          Brief description of commerce benefits across up to
+                          two lines
+                        </span>
+                      </div>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      <TempIcon iconSize="56" />
+                      <div>
+                        Managed Services
+                        <br />
+                        <span className="dropdown-item-content">
+                          Brief description of commerce benefits across up to
+                          two lines
+                        </span>
+                      </div>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      <TempIcon iconSize="56" />
+                      <div>
+                        Bella Health
+                        <br />
+                        <span className="dropdown-item-content">
+                          Brief description of commerce benefits across up to
+                          two lines
+                        </span>
+                      </div>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item dropdown-link-blue" href="#">
+                      See Pricing & Packages
+                    </Link>
+                  </li>
+                </div>
               </div>
             </div>
-          </div>
-        </ul>
-      </li>
+          </ul>
+        </li>
+
+        <li className="nav-item dropdown" style={{ position: 'static' }}>
+          <a
+            className="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Resources
+          </a>
+          <ul
+            className="dropdown-menu"
+            aria-labelledby="navbarDropdown"
+            style={{ width: '770px' }}
+          >
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-5">
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      Developer Center <TempIcon iconSize="12" />
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      Knowledge Center <TempIcon iconSize="12" />
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      Security
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      Blog
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      Events
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      Customer Case Studies
+                    </Link>
+                  </li>
+                </div>
+                <div className="col-lg-7 dropdown-section-gray">
+                  <p className="dropdown-section-title">Featured Resource</p>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      <TempIcon iconSize="150" />
+                      <div>
+                        <span className="dropdown-featured-title">
+                          On-Demand Webinar
+                        </span>
+                        <br />
+                        <span className="">
+                          Reimagining communication in the travel and
+                          hospitality industry
+                          <br />
+                          <span className="dropdown-link-blue">
+                            Watch Now <TempIcon iconSize="12" />
+                          </span>
+                        </span>
+                      </div>
+                    </Link>
+                  </li>
+                </div>
+              </div>
+            </div>
+          </ul>
+        </li>
+
+        <li className="nav-item dropdown" style={{ position: 'static' }}>
+          <a
+            className="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Support
+          </a>
+          <ul
+            className="dropdown-menu"
+            aria-labelledby="navbarDropdown"
+            style={{ width: '300px' }}
+          >
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-12">
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      Customer Success
+                      <br />
+                      <span className="dropdown-item-content">
+                        Brief description of commerce benefits across up to two
+                        lines
+                      </span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      Professional Services <br />
+                      <span className="dropdown-item-content">
+                        Brief description of commerce benefits across up to two
+                        lines
+                      </span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      Technical Support <br />
+                      <span className="dropdown-item-content">
+                        Brief description of commerce benefits across up to two
+                        lines
+                      </span>
+                    </Link>
+                  </li>
+                </div>
+              </div>
+            </div>
+          </ul>
+        </li>
+
+        {/*  <li className="nav-item dropdown">
+         <a
+           className="nav-link dropdown-toggle"
+           href={path}
+           data-bs-toggle="dropdown"
+         >
+           {label}
+         </a>
+         <ul className="dropdown-menu">
+           {children.map((item, index) => (
+             <li>
+               <Link
+                 className="dropdown-item"
+                 to={item.path}
+                 target={item.target}
+               >
+                 {item.label}
+               </Link>
+             </li>
+           ))}
+         </ul>
+       </li> */}
+      </>
     );
   }
   return (
