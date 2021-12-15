10b124574cc0f94372f5d3a92888afc9d7f37af0
Robert Lester <robertlesterjr@mac.com>
2021-12-15 16:12:27 +0000

    Change for tracking #2

--- wordpress/www/wp-config.php
+++ wordpress/www/wp-config.php
@@ -55,6 +55,7 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
+ 
 define( 'AUTH_KEY',         'put your unique phrase here' );
 define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
 define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
