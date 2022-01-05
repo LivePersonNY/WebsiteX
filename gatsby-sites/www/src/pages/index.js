adb6fee98a9f576fe847d6e3e5fbbf5bd4c6f02b
Giancarlo Trentini <giancarlo.trentini@gmail.com>
2022-01-05 16:16:07 -0500

    cleaning css

--- gatsby-sites/www/src/pages/index.js
+++ gatsby-sites/www/src/pages/index.js
@@ -3,19 +3,31 @@ import { Link, graphql } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
 import queryString from 'query-string';
 
+import Parser from 'html-react-parser';
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
-import Parser from 'html-react-parser';
 
 const IndexPage = ({ data: { page } }) => (
   <Layout>
-	<Seo title="Home" />
-	<div className="container">
-	  <div className="row align-items-center">
-	  <h1>HomePage</h1>
-		{Parser(page.content)}
-	  </div>
-	</div>
+    <Seo title="Home" />
+    <div className="pane bg-neutral-84">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-lg-5">
+            <h1>LivePerson’s unique value proposition</h1>
+          </div>
+          <div className="col-lg-6 offset-lg-1">
+            test 2<br />
+            test 2<br />
+            test 2<br />
+            test 2<br />
+            test 2<br />
+          </div>
+
+          {/* {Parser(page.content)} */}
+        </div>
+      </div>
+    </div>
   </Layout>
 );
 
@@ -23,10 +35,10 @@ export default IndexPage;
 
 export const pageQuery = graphql`
   query HomepageId {
-	page: wpPage(isFrontPage: {eq: true}) {
-	  id
-	  content
-	  title
-	}
+    page: wpPage(isFrontPage: { eq: true }) {
+      id
+      content
+      title
+    }
   }
 `;
