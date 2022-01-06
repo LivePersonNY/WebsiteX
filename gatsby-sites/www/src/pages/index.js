711f9656283efd3d2bc1ca4f54a544fdffd06a14
Giancarlo Trentini <giancarlo.trentini@gmail.com>
2022-01-06 18:02:19 -0500

    hp

--- gatsby-sites/www/src/pages/index.js
+++ gatsby-sites/www/src/pages/index.js
@@ -38,7 +38,7 @@ const IndexPage = ({ data: { page } }) => (
     </div>
 
     <div className="pane bg-primary-light">
-      <div className="container">
+      <div className="container comp-left-right">
         <div className="row align-items-center">
           <div className="col-lg-8">
             <h2>What is LivePerson?</h2>
@@ -58,6 +58,7 @@ const IndexPage = ({ data: { page } }) => (
             <img src="https://placekitten.com/640/480" alt="" />
           </div>
           <div className="col-lg-6">
+            <h6>UNDERSTAND</h6>
             <h2>
               Breathe life into your brand with human centered AI that’s
               anything but artificial.
@@ -74,6 +75,7 @@ const IndexPage = ({ data: { page } }) => (
             <img src="https://placekitten.com/640/480" alt="" />
           </div>
           <div className="col-lg-6">
+            <h6>CONNECT</h6>
             <h2>
               Deepen your understanding of consumers when they feel seen, heard
               and valued.
@@ -90,6 +92,7 @@ const IndexPage = ({ data: { page } }) => (
             <img src="https://placekitten.com/640/480" alt="" />
           </div>
           <div className="col-lg-6">
+            <h6>OUTCOMES</h6>
             <h2>
               Manage consumer conversations simply. Like, seriously simply.
             </h2>
@@ -105,18 +108,93 @@ const IndexPage = ({ data: { page } }) => (
 
     <div className="pane bg-primary-light">
       <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <h2 className="text-center">
+              The infinite power of conversational AI
+            </h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="comp-4col-grid bg-neutral-92 text-center">
+              <div className="row">
+                <div className="col-lg-3">
+                  <p>one</p>
+                </div>
+                <div className="col-lg-3">
+                  <p>two</p>
+                </div>
+                <div className="col-lg-3">
+                  <p>three</p>
+                </div>
+                <div className="col-lg-3">
+                  <p>four</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div className="pane bg-primary-light">
+      <div className="container comp-left-right">
         <div className="row align-items-center">
-          <div className="col-lg-8">
-            <h2>What is LivePerson?</h2>
+          <div className="col-lg-5">
+            <h6>PRE-HEADER TEXT</h6>
+            <h2>LivePerson for your business</h2>
+            <img src="https://placekitten.com/18/18" alt="" />
+            <h3>Commerce</h3>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci,
-              pellentesque a tincidunt dignissim adipiscing blandit eu. Sagittis
-              a, lorem eget nulla porttitor vitae ultricies. At aliquet non
-              viverra scelerisque enim sagittis.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat
+              eros, est dolor ullamcorper dui tellus eleifend.
             </p>
-            <Link className="link link-mt-large" href="#">
-              Discover Conversational AI
+            <Link className="link link-mt-small" href="#">
+              Discover Commerce Solutions
             </Link>
+            <img src="https://placekitten.com/18/18" alt="" />
+            <h3>Customer Care</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat
+              eros, est dolor ullamcorper dui tellus eleifend.
+            </p>
+            <Link className="link link-mt-small" href="#">
+              Discover Customer Care
+            </Link>
+          </div>
+          <div className="col-lg-6 offset-lg-1">
+            <img src="https://placekitten.com/640/720" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div className="pane bg-neutral-92">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 offset-lg-2">
+            <h2 className="text-center">
+              A sentence describing the different products and main value
+              proposition
+            </h2>
+          </div>
+        </div>
+        <div className="row bg-primary-light align-items-center">
+          <div className="col-lg-4 offset-lg-1">
+            <img src="https://placekitten.com/56/56" alt="" />
+            <h3>A branded line of copy or two about product benefit</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud.
+            </p>
+            <Link className="btn btn-outline-secondary" href="#">
+              Discover Customer Care
+            </Link>
+          </div>
+          <div className="col-lg-6 offset-lg-1">
+            <img src="https://placekitten.com/640/560" alt="" />
           </div>
         </div>
       </div>
