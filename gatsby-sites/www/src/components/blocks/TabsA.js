087517becd1f1d0f5a6726d43cf45638342b61fb
Giancarlo Trentini <giancarlo.trentini@gmail.com>
2022-01-12 18:14:40 -0500

    TabsA

--- gatsby-sites/www/src/components/blocks/TabsA.js
+++ gatsby-sites/www/src/components/blocks/TabsA.js
@@ -26,24 +26,14 @@ const TabsA = (props) => {
       return (
         <>
           <div class="accordion-item">
-            <h4 class="accordion-header" id="flush-headingOne">
-              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+            <h4 class="accordion-header" id={`flush-heading${index}`}>
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={`#flush-collapse${index}`} aria-expanded="false" aria-controls={`flush-collapse${index}`}>
                 {item}
               </button>
             </h4>
-            <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-              <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+            <div id={`flush-collapse${index}`} class="accordion-collapse collapse" aria-labelledby={`flush-heading${index}`} data-bs-parent="#tabsAAccordion">
+              <div class="accordion-body">{props.content[index]}</div>
             </div>
-          </div>
-          <div className="bg-primary-light comp-tabs-content" data-tab-content={index}>
-            <p className="h6">{item}</p>
-            <h4>{props.contentHeader[index]}</h4>
-            <p>{props.content[index]}</p>
-            {props.linkText[index] && (
-              <a className="link link-mt-large" href={props.linkUrl[index]}>
-                {props.linkText[index]}
-              </a>
-            )}
           </div>
         </>
       )
@@ -62,13 +52,13 @@ const TabsA = (props) => {
         <div className="row align-items-center">
           <div className="col-lg-4">
             <div className="comp-content-container">
-              <div class="accordion accordion-flush">
-
+              <div class="accordion accordion-flush" id="tabsAAccordion">
+                {tabContent}
               </div>
             </div>
           </div>
           <div className="col-lg-7 offset-lg-1">
-            {/* {tabImgOutput} */}
+            {tabImgOutput}
           </div>
         </div>
       </div>
