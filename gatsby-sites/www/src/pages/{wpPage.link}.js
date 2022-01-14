b408c9b6ce4fc954e31c7dbd402cf663bd13af5c
Robert Lester <robertlesterjr@mac.com>
2022-01-14 22:23:07 +0000

    ..

--- gatsby-sites/www/src/pages/{wpPage.link}.js
+++ gatsby-sites/www/src/pages/{wpPage.link}.js
@@ -8,9 +8,37 @@ import Hero from '../components/blocks/Hero';
 import Parser from 'html-react-parser';
 
 const PageTemplate = ({ data: { page } }) => {
+	
+	let meta = [
+		{
+		  name: `twitter:title`,
+		  content: page.seo.twitterTitle || ``,
+		},
+		{
+		  name: `twitter:description`,
+		  content: page.seo.twitterDescription || ``,
+		},
+		{
+		  property: `og:title`,
+		  content: page.seo.opengraphTitle || page.seo.title || page.title,
+		},
+		{
+		  property: `og:description`,
+		  content: page.seo.opengraphDescription || page.seo.metaDesc || ``,
+		},
+		{
+		  property: `og:image`,
+		  content: page.seo.opengraphImage ? page.seo.opengraphImage.mediaItemUrl : ``,
+		},
+		{
+		  property: `twitter:image`,
+		  content: page.seo.twitterImage ? page.seo.twitterImage.mediaItemUrl : ``,
+		}
+	];
+	
 	if (!page) return "The slug does not exist in the CMS";
   return (<Layout>
-	<Seo title={page.title} />
+	<Seo title={page.seo.title} description={page.seo.metaDesc} meta={meta} />
 	{Parser(page.content)}
   </Layout>)
 };
@@ -23,6 +51,31 @@ export const pageQuery = graphql`
 		id
 		content
 		title
+		seo {
+		  canonical
+		  cornerstone
+		  focuskw
+		  metaDesc
+		  metaKeywords
+		  metaRobotsNofollow
+		  metaRobotsNoindex
+		  opengraphAuthor
+		  opengraphDescription
+		  opengraphModifiedTime
+		  opengraphPublishedTime
+		  opengraphPublisher
+		  opengraphSiteName
+		  opengraphTitle
+		  opengraphType
+		  opengraphUrl
+		  readingTime
+		  title
+		  twitterDescription
+		  twitterTitle
+		  opengraphImage {
+			mediaItemUrl  
+		  }
+		}
 	  }
 	}
 `;
