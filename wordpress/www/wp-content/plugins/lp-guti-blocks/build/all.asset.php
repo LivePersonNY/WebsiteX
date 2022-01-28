124782fb7da21ac9b5a234e8adb445e3a6e9b0a2
Robert Lester <robertlesterjr@mac.com>
2022-01-28 19:54:03 +0000

    ..

--- wordpress/www/wp-content/plugins/lp-guti-blocks/build/all.asset.php
+++ wordpress/www/wp-content/plugins/lp-guti-blocks/build/all.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '7e52eed8ce225bdc8628f5edb7bc6ac9');
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '80c71b16c925ad0b19dfd795a39a50ef');
