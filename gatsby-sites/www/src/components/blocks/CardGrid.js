a1f1b92b5f8b04ceb4d88cb2d084426013a9b0d8
Giancarlo Trentini <giancarlo.trentini@gmail.com>
2022-02-08 11:17:17 -0500

    cardgrid

--- gatsby-sites/www/src/components/blocks/CardGrid.js
+++ gatsby-sites/www/src/components/blocks/CardGrid.js
@@ -7,49 +7,23 @@ import $ from 'jquery';
 
 const CardGrid = (props) => {
 
-  let tabScript = `
-    let items = document.querySelectorAll('#cardGridCarousel.carousel .carousel-item');
-
-    items.forEach((el) => {
-      const minPerSlide = 4;
-      let next = el.nextElementSibling;
-      for (var i=1; i != minPerSlide; i++) {
-        if (!next) {
-            // wrap carousel by using first child
-            next = items[0];
-        }
-        let cloneChild = next.cloneNode(true);
-        el.appendChild(cloneChild.children[0]);
-        next = next.nextElementSibling;
-      }
-    });
-  `;
-
-  if (props.runFilters) {
-    useEffect(() => {
-      eval(tabScript);
-    });
-  }
-
   let cardCol = props.items.map((item, index)=>{
     return (
-      <div className={`carousel-item ${index === 0 ? 'active' : ''}`}>
-        <div className="col-lg-4" key={index} id={index}>
-          <div className="card h-100">
-            <div className="card-body">
-              {!item.imgCtl && <img
-                className="card-image-internal"
-                src={item.imgSrc}
-                alt={item.imgAlt}
-              /> || item.imgCtl}
-              
-              <p className="card-text quote1">{item.body}</p>
-            </div>
-            <div className="card-footer">
-              <a href={item.linkUrl} className="card-link link">
-                {item.linkText}
-              </a>
-            </div>
+      <div className="col-lg" key={index} id={index}>
+        <div className="card h-100">
+          <div className="card-body">
+            {!item.imgCtl && <img
+              className="card-image-internal"
+              src={item.imgSrc}
+              alt={item.imgAlt}
+            /> || item.imgCtl}
+            
+            <p className="card-text quote1">{item.body}</p>
+          </div>
+          <div className="card-footer">
+            <a href={item.linkUrl} className="card-link link">
+              {item.linkText}
+            </a>
           </div>
         </div>
       </div>
@@ -75,24 +49,9 @@ const CardGrid = (props) => {
         )}
         <div className="row comp-card-grid-container">
           {props.sorter}
-          <div id="cardGridCarousel" className="carousel slide" data-bs-ride="carousel">
-            <button className="carousel-control-prev" type="button" data-bs-target="#cardGridCarousel" data-bs-slide="prev">
-              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#cardGridCarousel" data-bs-slide="next">
-              <span className="carousel-control-next-icon" aria-hidden="true"></span>
-              <span className="visually-hidden">Next</span>
-            </button>
-				    <div className="carousel-inner" role="listbox">
-              {cardCol}
-            </div>
-          </div>
+          {cardCol}
         </div>
       </div>
-      {!props.runFilters && (
-        <script>{tabScript}</script>
-      )}
     </div>
   );
 };
