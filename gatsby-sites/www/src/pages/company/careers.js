8b13c962c92ca750383b56aac3b3eca6fd6b2abb
Giancarlo Trentini <giancarlo.trentini@gmail.com>
2022-02-09 18:32:50 -0500

    careers

--- gatsby-sites/www/src/pages/company/careers.js
+++ gatsby-sites/www/src/pages/company/careers.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 import { withPrefix, Link, graphql } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
 import queryString from 'query-string';
@@ -7,9 +8,89 @@ import Helmet from "react-helmet"
 import Layout from '../../components/Layout';
 import Seo from '../../components/Seo';
 import CardGridB from '../../components/blocks/CardGridB';
-import '../../../careers-script';
 
-const CareersPage = () => (
+const CareersPage = () => {
+
+  let careersScript = `
+  console.log('careers script loaded4');
+
+  window.onload = (e) => {
+   document.querySelectorAll('.comp-body-container a').forEach(function(lnk) {
+       var href = lnk.href.concat("&gh_src=").concat(ghsrc ?? "");
+        lnk.href = href;
+   });
+   document.querySelectorAll('resume-upload').forEach(function(lnk) {
+       var href = lnk.href.concat("&gh_src=").concat(ghsrc ?? "");
+        lnk.href = href;
+   });
+   
+   let searchURL = 'http://careers.liveperson.com?source=campaignA'
+   .concat("&gh_src=").concat(ghsrc ?? "");
+   let keyWordQuery = document.getElementById('Job_Search').value;
+   if (keyWordQuery !== '') {
+       searchURL = searchURL.concat("&q=").concat(encodeURIComponent(keyWordQuery))
+   }
+   let submit = document.getElementById('Search_Submit_Button');
+   submit.href = searchURL;
+  };
+
+  function handleKey(e) {
+  e.preventDefault()
+  e.stopPropagation()
+
+  if (e.keyCode === 13) {
+    let searchURL = 'http://careers.liveperson.com?source=campaignA'.concat("&gh_src=")
+        .concat(ghsrc ?? "");
+    let keyWordQuery = document.getElementById('Job_Search').value;
+    if (keyWordQuery !== '') { 
+      sendGA();
+      searchURL = searchURL.concat("&q=")
+          .concat(encodeURIComponent(keyWordQuery))
+          
+
+    }
+    location.href = searchURL
+  }
+  }
+  function sendGA() {
+    ga('send', 'event', 'careers', 'click', 'Job search');
+  }
+
+  function setURLParams(e) {
+  let keyWordQuery = document.getElementById('Job_Search').value;
+
+  let searchURLParams = "https://careers.liveperson.com/?source=campaignA&q=";
+  searchURLParams = searchURLParams.concat(encodeURIComponent(keyWordQuery))
+      .concat("&gh_src=").concat(ghsrc ?? "");
+
+  let a = document.getElementById('Search_Submit_Button');
+      let b = document.getElementById('wf-form-Search-Form');
+  
+  a.href = searchURLParams;
+  b.setAttribute('action', searchURLParams);
+  }
+
+  document.getElementById('Job_Search').addEventListener('keyup', handleKey);
+  document.getElementById('wf-form-Search-Form').addEventListener('keyup', handleKey);
+  document.getElementById('Job_Search').addEventListener('input', setURLParams);
+
+  document.querySelector('.resume-upload').addEventListener('click',function(){
+  ga('send', 'event', 'careers', 'click', 'Match resume');
+  })
+
+  document.querySelector('.hero-or').addEventListener('click',function(){
+  console.log('click test');
+  })
+
+  console.log('end of careers script');
+  `;
+
+  useEffect(() => {
+    eval(careersScript);
+  });
+
+ 
+  return (
   <Layout mainClass="company-careers">
     {/* <Helmet>
       <script src={withPrefix('scripts/careers.js')} type="text/javascript" data-type="pageScript" />
@@ -106,6 +187,7 @@ const CareersPage = () => (
     />
 
   </Layout>
-);
+)
+    }
 
 export default CareersPage;
