25ba2401087f05d3ad7b2b6a31b2b180c6097def
Giancarlo Trentini <giancarlo.trentini@gmail.com>
2022-02-10 15:59:28 -0500

    404

--- gatsby-sites/www/src/pages/404.js
+++ gatsby-sites/www/src/pages/404.js
@@ -1,53 +1,27 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+import Layout from '../components/Layout';
+import Seo from '../components/Seo';
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <Layout>
+      <Seo title="404 Error" />
+
+      <div className="pane comp-plain-content bg-neutral-96 text-center pane-with-lead-text">
+        <div className="container">
+          <div className="row align-items-center justify-content-center">
+            <div className="col-lg-8">
+              <p className="h6">404 ERROR</p>
+              <h1>Oops, we can’t seem to find the page you are looking for</h1>
+              <p>Our Conversational Cloud platform empowers millions of people by allowing them to directly message their favorite brands. Join our team and be a part of the world’s next digital evolution.</p>
+              <a className="link link-mt-large" href="/">Go to Homepage</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
   )
 }
 
