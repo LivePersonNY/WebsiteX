6b6c99a858ab77640424a8754c35dfded842be9c
Robert Lester <robertlesterjr@mac.com>
2022-02-10 16:15:51 +0000

    packaged mods.

--- wordpress/www/wp-content/plugins/lp-guti-blocks/build/all.asset.php
+++ wordpress/www/wp-content/plugins/lp-guti-blocks/build/all.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'cd33f3ce3b017c22efde78181ce4382e');
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '97c6d2ff16b88b3b0dfeb5ce889d54de');
