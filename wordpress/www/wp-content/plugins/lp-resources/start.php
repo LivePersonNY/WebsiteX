f72c7f0181bd31ae3072b45b210d90a0117a6c53
Robert Lester <robertlesterjr@mac.com>
2022-02-24 04:28:20 +0000

    ..

--- wordpress/www/wp-content/plugins/lp-resources/start.php
+++ wordpress/www/wp-content/plugins/lp-resources/start.php
@@ -25,6 +25,15 @@ class LP_Resources
 		add_filter('get_custom_logo_image_attributes', [$this, 'logo_class']);
 		
 		add_filter('gatsby_action_monitors', [$this, 'filter_gatsby_hooks']);
+		
+		//add_filter('simple_local_avatar', [$this, 'filter_avatar']);
+	}
+	
+	function filter_avatar($avatar)
+	{
+		var_dump($avatar);
+		exit;
+		return $avatar;
 	}
 	
 	function filter_gatsby_hooks($actions)
