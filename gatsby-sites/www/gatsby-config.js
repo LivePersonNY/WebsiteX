445bb9b660c7fe4180ba2acd3771f5b4c86e99f5
Robert Lester <robertlesterjr@mac.com>
2022-02-28 22:53:04 +0000

    fix the css on the blog.

--- gatsby-sites/www/gatsby-config.js
+++ gatsby-sites/www/gatsby-config.js
@@ -74,7 +74,7 @@ module.exports = {
       resolve: `gatsby-plugin-purgecss`,
       options: {
         printRejected: true, // Print removed selectors and processed file names
-        // develop: true, // Enable while using `gatsby develop`
+        develop: true, // Enable while using `gatsby develop`
         // tailwind: true, // Enable tailwindcss support
         // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
@@ -102,7 +102,11 @@ module.exports = {
             'voice',
             'cloud',
             'health',
-            'success'
+            'success',
+            'blockquote',
+            'figure',
+            '.wp-block-pullquote',
+            'cite',
           ], // Don't remove this selector
         },
         // More options defined here https://purgecss.com/configuration.html#options
