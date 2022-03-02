8501c000ed35d129ff0808eb8f9d28c2195dae4f
Robert Lester <robertlesterjr@mac.com>
2022-03-02 15:39:25 +0000

    add preview flag.

--- gatsby-sites/www/gatsby-config.js
+++ gatsby-sites/www/gatsby-config.js
@@ -114,7 +114,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-env-variables`,
       options: {
-        allowList: ["CAN_ROOT", "NODE_ENV", "BRANCH"]
+        allowList: ["CAN_ROOT", "NODE_ENV", "BRANCH", "GATSBY_IS_PREVIEW"]
       },
     },
     {
