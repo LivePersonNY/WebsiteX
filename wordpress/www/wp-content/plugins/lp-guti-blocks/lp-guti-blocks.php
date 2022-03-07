355b0bee3c96b2c4b82716849b41dcd806337ba6
Robert Lester <robertlesterjr@mac.com>
2022-03-07 16:46:09 +0000

    ..

--- wordpress/www/wp-content/plugins/lp-guti-blocks/lp-guti-blocks.php
+++ wordpress/www/wp-content/plugins/lp-guti-blocks/lp-guti-blocks.php
@@ -85,3 +85,11 @@ function lp_mime_types($mimes) {
 }
 
 add_filter('upload_mimes', 'lp_mime_types');
+
+add_filter( 'post_thumbnail_html', 'lp_remove_width_attribute', 10 );
+add_filter( 'image_send_to_editor', 'lp_remove_width_attribute', 10 );
+
+function lp_remove_width_attribute( $html ) {
+   $html = preg_replace( '/(width|height)="\d*"\s/', "", $html );
+   return $html;
+}
