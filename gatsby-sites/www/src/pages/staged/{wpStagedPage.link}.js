56f6804f6f471c7c8dc7339e13d9ca3cdc52a4c6
Robert Lester <robertlesterjr@mac.com>
2022-04-04 19:05:46 +0000

    …..

--- gatsby-sites/www/src/pages/staged/{wpStagedPage.link}.js
+++ gatsby-sites/www/src/pages/staged/{wpStagedPage.link}.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { useState, useEffect } from 'react';
 import { Link, graphql } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
-import NotFoundPage from './404';
+import NotFoundPage from './../404';
 
 import Layout from '../../components/Layout';
 import Seo from '../../components/Seo';
