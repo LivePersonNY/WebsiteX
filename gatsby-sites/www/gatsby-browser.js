3f9e4e1cfc7aec4f9d12039fa77cb68f584ccc26
Robert Lester <robertlesterjr@mac.com>
2022-04-19 13:29:44 +0000

    ..

--- gatsby-sites/www/gatsby-browser.js
+++ gatsby-sites/www/gatsby-browser.js
@@ -5,4 +5,15 @@ import './src/resources/scss/index.scss';
 
 export const onRouteUpdate = () => {
   console.log("onRouteUpdate") // this works
+  
+  window.Localize.initialize({
+    key: `${process.env.LOCALIZE_KEY}`,
+    saveNewPhrasesFromSource: true,
+    retranslateOnNewPhrases: true,
+    translateMetaTags: true,
+    blockedClasses: [
+      'lp-window-root',
+      'microMode'
+    ],
+  });
 }
