21b4837fd354e02c924d1cbdf16aefd5766ad27f
Robert Lester <robertlesterjr@mac.com>
2022-07-14 13:24:44 +0000

    scripts.

--- gatsby-sites/roi/gatsby-browser.js
+++ gatsby-sites/roi/gatsby-browser.js
@@ -1,11 +1,12 @@
+import $ from 'jquery';
 import bootstrap from 'bootstrap';
 
 import './liveperson-scripts';
+import './roi-implemented';
 
 import './src/resources/scss/index.scss';
 
-// import './roi-calc';
-import './roi-implemented';
+//
 
 export const onRouteUpdate = () => {
   console.log("onRouteUpdate") // this works
