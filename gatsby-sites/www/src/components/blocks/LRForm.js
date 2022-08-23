e80b4d205d69e02f849b0aa81efecd60cd44c672
Robert Lester <robertlesterjr@mac.com>
2022-08-23 13:31:47 +0000

    …

--- gatsby-sites/www/src/components/blocks/LRForm.js
+++ gatsby-sites/www/src/components/blocks/LRForm.js
@@ -73,7 +73,7 @@ const LRForm = (props) => {
         <div className="container">
           <div className="row align-items-center">
             <div
-              className={`col-lg-6 ${props.flipColumns ? 'order-lg-last' : 'order-last'}`}
+              className={`col-lg-6 ${props.flipColumns ? 'order-last' : 'order-lg-last'}`}
             >
               {
                 !props.imgCtl && (
@@ -91,7 +91,7 @@ const LRForm = (props) => {
             </div>
             <div
               className={`col-lg-6 ${
-                props.flipColumns ? 'order-lg-first' : 'order-first'
+                props.flipColumns ? 'order-first' : 'order-lg-first'
               }`}
             >
               {props.kicker && <p className="h6 text-uppercase">{props.kicker}</p>}
--- wordpress/www/wp-content/plugins/lp-guti-blocks/build/all.asset.php
+++ wordpress/www/wp-content/plugins/lp-guti-blocks/build/all.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '92d9dc43b84ce9ed1803b8ffee202419');
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '63a3cf4da8af5f7d8ce29af13e46f464');
