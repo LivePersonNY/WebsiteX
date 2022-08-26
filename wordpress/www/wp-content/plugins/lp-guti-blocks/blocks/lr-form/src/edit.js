e5065cc32f09e40b0bb810d912c47cc67e5f9905
Robert Lester <robertlesterjr@mac.com>
2022-08-26 15:04:52 +0000

    …

--- wordpress/www/wp-content/plugins/lp-guti-blocks/blocks/lr-form/src/edit.js
+++ wordpress/www/wp-content/plugins/lp-guti-blocks/blocks/lr-form/src/edit.js
@@ -4,7 +4,7 @@
  * @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
  */
 import { __ } from '@wordpress/i18n';
-
+import $ from 'jquery';
 /**
  * React hook that is used to mark the block wrapper element.
  * It provides all the necessary props like the class name.
--- wordpress/www/wp-content/plugins/lp-guti-blocks/build/all.asset.php
+++ wordpress/www/wp-content/plugins/lp-guti-blocks/build/all.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f54407c26fe41c73f41a38dc8af685db');
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '02ae30e3a8ffd77e89ecf6d079bb6e06');
