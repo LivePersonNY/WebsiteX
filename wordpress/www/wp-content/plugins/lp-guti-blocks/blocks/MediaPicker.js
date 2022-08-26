a46bf09f3501c01d41f9ddfa09e41567960c0571
Robert Lester <robertlesterjr@mac.com>
2022-08-26 14:56:00 +0000

    ….

--- wordpress/www/wp-content/plugins/lp-guti-blocks/blocks/MediaPicker.js
+++ wordpress/www/wp-content/plugins/lp-guti-blocks/blocks/MediaPicker.js
@@ -36,7 +36,7 @@ export default function MediaPicker({setAttributes, attributes, allowLottie, css
 					allowedTypes={ allowLottie ? ['image', 'application/json', 'application/zip'] : ['image'] }
 					render={({open}) => (
 						<>
-							{!attributes.lottieFile && <img className={`imageSelector ${cssClass}`} src={attributes.mediaUrl} onClick={open} /> ||
+							{allowLottie && !attributes.lottieFile && <img className={`imageSelector ${cssClass}`} src={attributes.mediaUrl} onClick={open} /> ||
 								<Button variant="link" onClick={open}>Select Image</Button>}
 
 							{attributes.lottieFile && <LottieFilePlayer className={`imageSelector ${cssClass} ${attributes.lottieId}`} onClick={open} lottieFile={attributes.lottieFile} autoplay={true} loop={true} />}
