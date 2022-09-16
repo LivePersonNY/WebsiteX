b71b33ec4e3f4137e6f5eb532bd7a3b88c1dc799
Giancarlo Trentini <giancarlo.trentini@gmail.com>
2022-09-16 15:49:44 -0400

    features slider links target

--- wordpress/www/wp-content/plugins/lp-guti-blocks/build/all.asset.php
+++ wordpress/www/wp-content/plugins/lp-guti-blocks/build/all.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e4c761a2d1fa5da3e286f964eed84239');
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ec3f9fe66ec2d2bd5e538c6335623025');
