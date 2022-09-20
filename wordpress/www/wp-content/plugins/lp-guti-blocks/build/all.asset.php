5f68e5884bb95f2b5dbc9ffb1198471e3d249eea
Giancarlo Trentini <giancarlo.trentini@gmail.com>
2022-09-20 16:45:15 -0400

    roicalcwp build

--- wordpress/www/wp-content/plugins/lp-guti-blocks/build/all.asset.php
+++ wordpress/www/wp-content/plugins/lp-guti-blocks/build/all.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ec3f9fe66ec2d2bd5e538c6335623025');
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '71cfcc78d094cea78054809c052de6c3');
