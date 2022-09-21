841d589a99f2dc055af40e4cbe3f541e2e2fcd8e
Giancarlo Trentini <giancarlo.trentini@gmail.com>
2022-09-21 18:22:49 -0400

    roi page template

---
+++ gatsby-sites/www/src/templates/Roi.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { Link, graphql } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
+
+import Layout from '../components/Layout';
+import Seo from '../components/Seo';
+import Hero from '../components/blocks/Hero';
+import Parser from 'html-react-parser';
+
+const Roi = ({ pageContext: {page}}) => {
+
+	if (process.env.BRANCH != 'develop' && process.env.GATSBY_IS_PREVIEW !== "true") {
+		return (<NotFoundPage />);
+	}
+
+	useEffect(() => {
+    
+		window.runRoi();
+		
+	});
+	
+	let canRoot = process.env.CAN_ROOT;
+	let canonical = page.seo.canonical || page.link;
+	if (canonical.indexOf('http') < 0) canonical = canRoot + canonical;
+	
+	if (!page) return "The slug does not exist in the CMS";
+  return (
+  <Layout>
+	<Seo title={page.seo.title} description={page.seo.metaDesc} canonical={canonical} robots="noindex, nofollow" />
+	{Parser(page.content)}
+  </Layout>)
+};
+
+export default Roi;
