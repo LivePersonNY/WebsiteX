606c0f3f36c2fb84356dac498404e5d8e98ba57e
Giancarlo Trentini <giancarlo.trentini@gmail.com>
2022-12-01 15:42:27 -0500

    blog bio styles

--- wordpress/www/wp-content/plugins/lp-guti-blocks/build/all.asset.php
+++ wordpress/www/wp-content/plugins/lp-guti-blocks/build/all.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9a919fb6ac73288670688009d9e92aeb');
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'de3fde5d7f696b242f5a17b8f9f6c8b7');
