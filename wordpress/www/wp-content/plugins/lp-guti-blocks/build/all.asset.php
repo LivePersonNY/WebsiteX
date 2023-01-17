8e45f45ef969b2990835b96351bcee7c58f85866
Giancarlo Trentini <giancarlo.trentini@gmail.com>
2023-01-17 12:16:57 -0500

    dunno, wp build wasnt working, now is

--- wordpress/www/wp-content/plugins/lp-guti-blocks/build/all.asset.php
+++ wordpress/www/wp-content/plugins/lp-guti-blocks/build/all.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'de3fde5d7f696b242f5a17b8f9f6c8b7');
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e734f1d1018e4541507a');
