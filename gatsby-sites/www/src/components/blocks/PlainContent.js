d4d55a4b011df046fa6b3b90499cafe3c7ad947b
Giancarlo Trentini <giancarlo.trentini@gmail.com>
2023-01-19 12:25:38 -0500

    plain content block

--- gatsby-sites/www/src/components/blocks/PlainContent.js
+++ gatsby-sites/www/src/components/blocks/PlainContent.js
@@ -34,99 +34,92 @@ const PlainContent = function (props) {
     };
 
     return (
-        <>
-            <div
-                data-localize={props.autoApprove && `auto-approve`}
-                autoapprove={props.autoApprove && 'true'}
-                id={props.anchor}
-                className={`pane comp-plain-content ${
-                    props.backgroundColor || 'bg-transparent'
-                } ${props.alignmentClass} ${
-                    props.header ? 'pane-with-lead-text' : ''
-                }`}
-            >
-                <div className="container">
-                    <div className="row align-items-center justify-content-center">
-                        <div className={`col-lg-${props.colWidth || 12}`}>
-                            {props.kicker && (
-                                <p className="h6 text-uppercase">
-                                    {props.kicker}
-                                </p>
-                            )}
-                            {headerLevel == 'h2' && (
-                                <h2>
-                                    {!props.animatedText && (
-                                        <Paragraph
-                                            text={props.header}
-                                            headerLevel={headerLevel}
-                                        />
-                                    )}
-                                    {props.animatedText && (
-                                        <Paragraph
-                                            text={fullAnimation()}
-                                            headerLevel={headerLevel}
-                                        />
-                                    )}
-                                </h2>
-                            )}
-                            {headerLevel == 'h1' && (
-                                <h1>
-                                    {!props.animatedText && (
-                                        <Paragraph
-                                            text={props.header}
-                                            headerLevel={headerLevel}
-                                        />
-                                    )}
-                                    {props.animatedText && (
-                                        <Paragraph
-                                            text={fullAnimation()}
-                                            headerLevel={headerLevel}
-                                        />
-                                    )}
-                                </h1>
-                            )}
-                            {props.body && <Paragraph text={props.body} />}
-                            {props.linkText && (
-                                <a
-                                    className="link link-mt-large"
-                                    href={props.linkUrl}
-                                >
-                                    {props.linkText}
-                                </a>
-                            )}
-                            {props.primaryBtnText && (
-                                <a
-                                    className="btn btn-primary"
-                                    href={props.primaryBtnLink}
-                                    target={props.linkExternal && `_blank`}
-                                    rel={
-                                        props.linkExternal &&
-                                        `noopener noreferrer`
-                                    }
-                                >
-                                    {props.primaryBtnText}
-                                </a>
-                            )}
-                            {props.secondaryBtnText && (
-                                <a
-                                    className="btn btn-outline-secondary"
-                                    href={props.secondaryBtnLink}
-                                    target={
-                                        props.linkSecondaryExternal && `_blank`
-                                    }
-                                    rel={
-                                        props.linkSecondaryExternal &&
-                                        `noopener noreferrer`
-                                    }
-                                >
-                                    {props.secondaryBtnText}
-                                </a>
-                            )}
-                        </div>
+        <div
+            data-localize={props.autoApprove && `auto-approve`}
+            autoapprove={props.autoApprove && 'true'}
+            id={props.anchor}
+            className={`pane comp-plain-content ${
+                props.backgroundColor || 'bg-transparent'
+            } ${props.alignmentClass} ${
+                props.header ? 'pane-with-lead-text' : ''
+            }`}
+        >
+            <div className="container">
+                <div className="row align-items-center justify-content-center">
+                    <div className={`col-lg-${props.colWidth || 12}`}>
+                        {props.kicker && (
+                            <p className="h6 text-uppercase">{props.kicker}</p>
+                        )}
+                        {headerLevel == 'h2' && (
+                            <h2>
+                                {!props.animatedText && (
+                                    <Paragraph
+                                        text={props.header}
+                                        headerLevel={headerLevel}
+                                    />
+                                )}
+                                {props.animatedText && (
+                                    <Paragraph
+                                        text={fullAnimation()}
+                                        headerLevel={headerLevel}
+                                    />
+                                )}
+                            </h2>
+                        )}
+                        {headerLevel == 'h1' && (
+                            <h1>
+                                {!props.animatedText && (
+                                    <Paragraph
+                                        text={props.header}
+                                        headerLevel={headerLevel}
+                                    />
+                                )}
+                                {props.animatedText && (
+                                    <Paragraph
+                                        text={fullAnimation()}
+                                        headerLevel={headerLevel}
+                                    />
+                                )}
+                            </h1>
+                        )}
+                        {props.body && <Paragraph text={props.body} />}
+                        {props.linkText && (
+                            <a
+                                className="link link-mt-large"
+                                href={props.linkUrl}
+                            >
+                                {props.linkText}
+                            </a>
+                        )}
+                        {props.primaryBtnText && (
+                            <a
+                                className="btn btn-primary"
+                                href={props.primaryBtnLink}
+                                target={props.linkExternal && `_blank`}
+                                rel={
+                                    props.linkExternal && `noopener noreferrer`
+                                }
+                            >
+                                {props.primaryBtnText}
+                            </a>
+                        )}
+                        {props.secondaryBtnText && (
+                            <a
+                                className="btn btn-outline-secondary"
+                                href={props.secondaryBtnLink}
+                                target={props.linkSecondaryExternal && `_blank`}
+                                rel={
+                                    props.linkSecondaryExternal &&
+                                    `noopener noreferrer`
+                                }
+                            >
+                                {props.secondaryBtnText}
+                            </a>
+                        )}
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
--- wordpress/www/wp-content/plugins/lp-guti-blocks/build/all.asset.php
+++ wordpress/www/wp-content/plugins/lp-guti-blocks/build/all.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '991f7fc50e7fb689172a');
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '80fd4fb80afce1615bf7');
