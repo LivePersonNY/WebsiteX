f642dd47549e16bff701b1dc85b15d0163b8226e
Giancarlo Trentini <giancarlo.trentini@gmail.com>
2023-01-27 16:59:38 -0500

    mkto prevpagepath

--- gatsby-sites/www/gatsby-browser.js
+++ gatsby-sites/www/gatsby-browser.js
@@ -5,25 +5,26 @@ import './liveperson-scripts';
 
 import './src/resources/scss/index.scss';
 
-
-
 export const onRouteUpdate = () => {
-  
-  
-  window.dataLayer = window.dataLayer || [];
-  window.runRoi = runRoi;
-  
-  const pagePath = location ? location.pathname + location.search + location.hash	: undefined;
-  window.ga && window.ga('set', 'page', pagePath);
-  window.ga && window.ga('send', 'pageview');
-  
-  if (location && window.Munchkin) {
-    window.Munchkin.munchkinFunction('visitWebPage', {
-        'url': location.pathname,
-        'params': location.search
-    });
-  }
-  
-  console.log("onRouteUpdate", pagePath) // this works
-  
-}
+    window.dataLayer = window.dataLayer || [];
+    window.runRoi = runRoi;
+
+    const pagePath = location
+        ? location.pathname + location.search + location.hash
+        : undefined;
+    window.ga && window.ga('set', 'page', pagePath);
+    window.ga && window.ga('send', 'pageview');
+
+    if (location && window.Munchkin) {
+        window.Munchkin.munchkinFunction('visitWebPage', {
+            url: location.pathname,
+            params: location.search,
+        });
+    }
+
+    window.locations = window.locations || [document.referrer];
+    locations.push(window.location.href);
+    window.previousPath = locations[locations.length - 2];
+
+    console.log('onRouteUpdate', pagePath); // this works
+};
--- gatsby-sites/www/src/html.js
+++ gatsby-sites/www/src/html.js
@@ -1,13 +1,12 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function HTML(props) {
-  
-  const gtm_id = process.env.GTM_ID;
-  const lzScript = `
-  
+    const gtm_id = process.env.GTM_ID;
+    const lzScript = `
+
       !function(a){if(!a.Localize){a.Localize={};for(var e=["translate","untranslate","phrase","initialize","translatePage","setLanguage","getLanguage","detectLanguage","getAvailableLanguages","untranslatePage","bootstrap","prefetch","on","off","hideWidget","showWidget","getSourceLanguage"],t=0;t<e.length;t++)a.Localize[e[t]]=function(){}}}(window);
-      
+
       Localize.initialize({
         key: '${process.env.LOCALIZE_KEY}',
         retranslateOnNewPhrases: true,
@@ -23,58 +22,55 @@ export default function HTML(props) {
           'gatsby-announcer'
         ]
       });
-      
+
   `;
-  
-  const gtmScript = `
+
+    const gtmScript = `
     (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','${gtm_id}');
   `;
-  
-  const frameStyle = {
-    display: 'none',
-    visibility: 'hidden'
-  };
-  
-  return (
-    <html {...props.htmlAttributes}>
-      <head>        
-        
-        <meta charSet="utf-8" />
-        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-        
-        <script src="https://global.localizecdn.com/localize.js"></script>
-        <script dangerouslySetInnerHTML={{ __html: lzScript }}></script>
-        
-        {props.headComponents}
-      </head>
-      <body {...props.bodyAttributes}>
-      
-        
-        {props.preBodyComponents}
-        <div
-          key={`body`}
-          id="___gatsby"
-          dangerouslySetInnerHTML={{ __html: props.body }}
-        />
-        {props.postBodyComponents}
-      </body>
-    </html>
-  )
+
+    const frameStyle = {
+        display: 'none',
+        visibility: 'hidden',
+    };
+
+    return (
+        <html {...props.htmlAttributes}>
+            <head>
+                <meta charSet="utf-8" />
+                <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1, shrink-to-fit=no"
+                />
+
+                <script src="https://global.localizecdn.com/localize.js"></script>
+                <script dangerouslySetInnerHTML={{ __html: lzScript }}></script>
+
+                {props.headComponents}
+            </head>
+            <body {...props.bodyAttributes}>
+                {props.preBodyComponents}
+                <div
+                    key={`body`}
+                    id="___gatsby"
+                    dangerouslySetInnerHTML={{ __html: props.body }}
+                />
+                {props.postBodyComponents}
+            </body>
+        </html>
+    );
 }
 
 HTML.propTypes = {
-  htmlAttributes: PropTypes.object,
-  headComponents: PropTypes.array,
-  bodyAttributes: PropTypes.object,
-  preBodyComponents: PropTypes.array,
-  body: PropTypes.string,
-  postBodyComponents: PropTypes.array,
-}
+    htmlAttributes: PropTypes.object,
+    headComponents: PropTypes.array,
+    bodyAttributes: PropTypes.object,
+    preBodyComponents: PropTypes.array,
+    body: PropTypes.string,
+    postBodyComponents: PropTypes.array,
+};
