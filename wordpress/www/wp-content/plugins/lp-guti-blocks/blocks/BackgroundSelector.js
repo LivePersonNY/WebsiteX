de91670dada77f6105bfe5cd9988891e9b7bdd52
Giancarlo Trentini <giancarlo.trentini@gmail.com>
2023-02-07 17:06:56 -0500

    plain content block bgcolor

--- wordpress/www/wp-content/plugins/lp-guti-blocks/blocks/BackgroundSelector.js
+++ wordpress/www/wp-content/plugins/lp-guti-blocks/blocks/BackgroundSelector.js
@@ -1,54 +1,60 @@
-import { ToolbarDropdownMenu } from '@wordpress/components';
+import { ToolbarDropdownMenu } from "@wordpress/components";
 
 export default function BackgroundSelectorMenu({ callback, selected }) {
-
-	return (<ToolbarDropdownMenu
-		icon="art"
-		label="Background"
-		controls={ [
-			{
-				title: 'None (Default)',
-				isActive: selected == 'bg-transparent',
-				icon: 'admin-customizer',
-				onClick: () => callback('bg-transparent')
-			},
-			{
-				title: 'Gradiant Image',
-				icon: 'admin-customizer bg-grad-image',
-				isActive: selected == 'bg-grad-image',
-				onClick: () => callback('bg-grad-image')
-			},
-			{
-				title: 'Rainbow',
-				icon: 'admin-customizer bg-rainbow',
-				isActive: selected == 'bg-rainbow',
-				onClick: () => callback('bg-rainbow')
-			},
-			{
-				title: 'Lavender',
-				icon: 'admin-customizer bg-lavendar-20',
-				isActive: selected == 'bg-lavendar-20',
-				onClick: () => callback('bg-lavendar-20')
-			},
-			{
-				title: 'Blue',
-				icon: 'admin-customizer bg-blue-20',
-				isActive: selected == 'bg-blue-20',
-				onClick: () => callback('bg-blue-20')
-			},
-			{
-				title: 'Gray',
-				icon: 'admin-customizer bg-neutral-96',
-				isActive: selected == 'bg-neutral-96',
-				onClick: () => callback('bg-neutral-96')
-			},
-			{
-				title: 'Light Gray',
-				icon: 'admin-customizer bg-primary-light',
-				isActive: selected == 'bg-primary-light',
-				onClick: () => callback('bg-primary-light')
-			}
-		] }
-	/>);
-
+	return (
+		<ToolbarDropdownMenu
+			icon="art"
+			label="Background"
+			controls={[
+				{
+					title: "None (Default)",
+					isActive: selected == "bg-transparent",
+					icon: "admin-customizer",
+					onClick: () => callback("bg-transparent"),
+				},
+				{
+					title: "Gradiant Image",
+					icon: "admin-customizer bg-grad-image",
+					isActive: selected == "bg-grad-image",
+					onClick: () => callback("bg-grad-image"),
+				},
+				{
+					title: "Rainbow",
+					icon: "admin-customizer bg-rainbow",
+					isActive: selected == "bg-rainbow",
+					onClick: () => callback("bg-rainbow"),
+				},
+				{
+					title: "Lavender",
+					icon: "admin-customizer bg-lavendar-20",
+					isActive: selected == "bg-lavendar-20",
+					onClick: () => callback("bg-lavendar-20"),
+				},
+				{
+					title: "Blue",
+					icon: "admin-customizer bg-blue-20",
+					isActive: selected == "bg-blue-20",
+					onClick: () => callback("bg-blue-20"),
+				},
+				{
+					title: "Gray",
+					icon: "admin-customizer bg-neutral-96",
+					isActive: selected == "bg-neutral-96",
+					onClick: () => callback("bg-neutral-96"),
+				},
+				{
+					title: "Light Gray",
+					icon: "admin-customizer bg-primary-light",
+					isActive: selected == "bg-primary-light",
+					onClick: () => callback("bg-primary-light"),
+				},
+				{
+					title: "Black",
+					icon: "admin-customizer bg-neutral-12",
+					isActive: selected == "bg-neutral-12",
+					onClick: () => callback("bg-neutral-12"),
+				},
+			]}
+		/>
+	);
 }
--- wordpress/www/wp-content/plugins/lp-guti-blocks/build/all.asset.php
+++ wordpress/www/wp-content/plugins/lp-guti-blocks/build/all.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '607aac13bf5c18d21493');
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'abba6986c64324e2c9ba');
