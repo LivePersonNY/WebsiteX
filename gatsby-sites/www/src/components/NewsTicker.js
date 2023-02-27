91d34348683191bd4aa7cc12375d849ffb9a7e40
Giancarlo Trentini <giancarlo.trentini@gmail.com>
2023-02-27 17:29:04 -0500

    news ticker updates

--- gatsby-sites/www/src/components/NewsTicker.js
+++ gatsby-sites/www/src/components/NewsTicker.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Link, graphql, useStaticQuery } from 'gatsby';
 
@@ -42,29 +42,54 @@ const NewsTicker = ({ siteTitle }) => {
         `
     );
 
+    useEffect(() => {
+        console.log('this is for news ticker');
+        const newsTickerContainer = document.querySelector(
+            '.news-ticker-container'
+        );
+        const newsTickerClose = document.querySelector('.news-ticker-close');
+        newsTickerClose.addEventListener('click', () => {
+            newsTickerContainer.style.transition = 'all 1s ease-in-out';
+            newsTickerContainer.style.maxHeight = '0px';
+            newsTickerContainer.style.padding = '0px';
+            newsTickerContainer.style.zIndex = '-2';
+            newsTickerClose.style.display = 'none';
+        });
+    });
+
     return (
         <>
             {newsTickerItems.nodes.length > 0 && (
                 <div className="news-ticker-container">
-                    {newsTickerItems.nodes.map((item, index) => {
-                        const cssClasses = item.cssClasses.length
-                            ? item.cssClasses.join(' ')
-                            : 'nav-link';
-                        return (
-                            <React.Fragment key={index}>
-                                <p className={cssClasses}>{item.description}</p>
-                                <a
-                                    target={item.target}
-                                    rel={item.target && `noopener noreferrer`}
-                                    title={item.title}
-                                    className="link"
-                                    href={item.path}
-                                >
-                                    {item.label}
-                                </a>
-                            </React.Fragment>
-                        );
-                    })}
+                    <div className="container">
+                        {newsTickerItems.nodes.map((item, index) => {
+                            const cssClasses = item.cssClasses.length
+                                ? item.cssClasses.join(' ')
+                                : 'nav-link';
+                            return (
+                                <React.Fragment key={index}>
+                                    <p className={cssClasses}>
+                                        {item.description}
+                                    </p>
+                                    <a
+                                        target={item.target}
+                                        rel={
+                                            item.target && `noopener noreferrer`
+                                        }
+                                        title={item.title}
+                                        className="link btn btn-outline-secondary"
+                                        href={item.path}
+                                    >
+                                        {item.label}
+                                    </a>
+                                </React.Fragment>
+                            );
+                        })}
+                        <img
+                            className="news-ticker-close"
+                            src="https://static.liveperson.com/static-assets/2023/02/27164937/cancel.png"
+                        />
+                    </div>
                 </div>
             )}
         </>
