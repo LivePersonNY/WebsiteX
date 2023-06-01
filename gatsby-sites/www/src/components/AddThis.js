d4d23b8666bfac9f0e3efe4bac917351d947003e
Giancarlo Trentini <giancarlo.trentini@gmail.com>
2023-06-01 14:27:16 -0400

    removing addThis

--- gatsby-sites/www/src/components/AddThis.js
+++ gatsby-sites/www/src/components/AddThis.js
@@ -3,34 +3,33 @@ import { useState, useEffect } from 'react';
 import Parser from 'html-react-parser';
 import Script from 'react-load-script';
 
-
 import { Link, graphql } from 'gatsby';
 
 export default function AddThis(props) {
-		
-	useEffect(() => {
-		if (window.addthis && typeof(window.addthis.layers.refresh) === 'function') {
-			window.addthis.layers.refresh();
-			window.addthis.update('share', 'url', props.url);
-		}
-	}, [props.url]);
-	
-	const handleAddthisLoaded = () => {
-		window.addthis.init();
-		window.addthis.update('share', 'url', props.url);
-	};
-	
-	return (
-		<>
-			{props.type == "share" && <div className="addthis_inline_share_toolbox"></div>}
+    useEffect(() => {
+        if (
+            window.addthis &&
+            typeof window.addthis.layers.refresh === 'function'
+        ) {
+            window.addthis.layers.refresh();
+            window.addthis.update('share', 'url', props.url);
+        }
+    }, [props.url]);
+
+    const handleAddthisLoaded = () => {
+        window.addthis.init();
+        window.addthis.update('share', 'url', props.url);
+    };
+
+    return (
+        <>
+            {/* {props.type == "share" && <div className="addthis_inline_share_toolbox"></div>}
 			{props.type == "related" && <div className="addthis_relatedposts_inline"></div>}
 			{props.type == "follow" && <div className="addthis_inline_follow_toolbox"></div>}
 			<Script
 				url="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-621685298b452c11"
 				onLoad={handleAddthisLoaded} />
-				
-			
-		</>
-	);
-	
+				 */}
+        </>
+    );
 }
