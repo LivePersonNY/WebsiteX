2c5d10416da0f383e924351992ddc016b560ea3c
Giancarlo Trentini <giancarlo.trentini@gmail.com>
2023-08-02 17:26:40 -0400

    plainContent animated text

--- gatsby-sites/www/src/components/blocks/PlainContent.js
+++ gatsby-sites/www/src/components/blocks/PlainContent.js
@@ -6,11 +6,11 @@ import Paragraph from '../Paragraph';
 const PlainContent = function (props) {
     let headerLevel = props.headLevel || 'h2';
 
-    let fullAnimation = () => {
-        let headerText = props.header;
+    let fullAnimation = (textContent, animatedContent) => {
+        let headerText = textContent;
         headerText = headerText.split('animatedText');
 
-        let headerAnimateText = props.animatedText;
+        let headerAnimateText = animatedContent;
         headerAnimateText = headerAnimateText.split(',');
         let animatedLoop = headerAnimateText.map((item, index) => {
             let isActive = '';
@@ -65,10 +65,22 @@ const PlainContent = function (props) {
                             props.assetTopCtl}
                         {props.kicker && (
                             <p className="h6 text-uppercase">
-                                <Paragraph
-                                    text={props.kicker}
-                                    headerLevel="nothing"
-                                />
+                                {!props.animatedKickerText && (
+                                    <Paragraph
+                                        text={props.kicker}
+                                        headerLevel="nothing"
+                                    />
+                                )}
+                                {props.animatedKickerText && (
+                                    <Paragraph
+                                        text={fullAnimation(
+                                            props.kicker,
+                                            props.animatedKickerText
+                                        )}
+                                        headerLevel="nothing"
+                                    />
+                                )}
+                                <Paragraph text={props.kicker} />
                             </p>
                         )}
                         {headerLevel == 'h2' && (
@@ -81,7 +93,10 @@ const PlainContent = function (props) {
                                 )}
                                 {props.animatedText && (
                                     <Paragraph
-                                        text={fullAnimation()}
+                                        text={fullAnimation(
+                                            props.header,
+                                            props.animatedContent
+                                        )}
                                         headerLevel={headerLevel}
                                     />
                                 )}
@@ -97,7 +112,10 @@ const PlainContent = function (props) {
                                 )}
                                 {props.animatedText && (
                                     <Paragraph
-                                        text={fullAnimation()}
+                                        text={fullAnimation(
+                                            props.header,
+                                            props.animatedContent
+                                        )}
                                         headerLevel={headerLevel}
                                     />
                                 )}
--- wordpress/www/wp-content/plugins/lp-guti-blocks/build/all.asset.php
+++ wordpress/www/wp-content/plugins/lp-guti-blocks/build/all.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '72f355a2291118d84556');
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '45b575799a34f482822d');
