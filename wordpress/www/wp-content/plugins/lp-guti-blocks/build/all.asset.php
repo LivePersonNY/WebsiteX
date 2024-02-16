e370fc8d59290a0f5785826c11626d7c61dc942e
Giancarlo Trentini <giancarlo.trentini@gmail.com>
2024-02-16 14:10:41 -0500

    overlay slider css

--- wordpress/www/wp-content/plugins/lp-guti-blocks/build/all.asset.php
+++ wordpress/www/wp-content/plugins/lp-guti-blocks/build/all.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a1dc94a3194553c08d1c');
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ae91ba1fe523d6f9172d');
