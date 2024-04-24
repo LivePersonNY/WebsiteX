dd604c1e15391728f4d15a679a66c6ecd970e6a5
Giancarlo Trentini <giancarlo.trentini@gmail.com>
2024-04-24 14:34:21 -0400

    netlify form; 3rd party scripts

--- gatsby-sites/www/static/custom-js/third-party-terms.js
+++ gatsby-sites/www/static/custom-js/third-party-terms.js
@@ -2,5 +2,5 @@ $(window).on('load', function (e) {
     console.log('page is fully loaded');
     $('.policy-toc').prepend('<h3>Section A</h3>');
 
-    $('.link.link-no-arrow[href="#web-messaging-voice-amp-videonbspcalls"]').parent().before('<h3>Section B</h3>');
+    $('.link.link-no-arrow[href="#web-messaging-voice-amp-videonbspcalls"]').parent().after('<h3>Section B</h3>');
 });
