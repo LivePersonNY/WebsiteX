0d64b9a94266fbc5b3d36ab1c152885bd3962180
Giancarlo Trentini <giancarlo.trentini@gmail.com>
2024-09-03 11:25:29 -0400

    policies

--- gatsby-sites/www/src/templates/Policy.js
+++ gatsby-sites/www/src/templates/Policy.js
@@ -7,22 +7,25 @@ import Seo from '../components/Seo';
 import Hero from '../components/blocks/Hero';
 import Parser from 'html-react-parser';
 
-const PageTemplate = ({ pageContext: {page}}) => {
+const PageTemplate = ({ pageContext: { page } }) => {
+    let canRoot = process.env.CAN_ROOT;
+    let canonical = page.seo.canonical || page.link;
+    if (canonical.indexOf('http') < 0) canonical = canRoot + canonical;
 
-	let canRoot = process.env.CAN_ROOT;
-	let canonical = page.seo.canonical || page.link;
-	if (canonical.indexOf('http') < 0) canonical = canRoot + canonical;
+    let robots = [page.seo.metaRobotsNoindex, page.seo.metaRobotsNofollow];
 
-	let robots = [
-		page.seo.metaRobotsNoindex,
-		page.seo.metaRobotsNofollow
-	];
-
-	if (!page) return "The slug does not exist in the CMS";
-  return (<Layout>
-	<Seo title={page.seo.title} description={page.seo.metaDesc} canonical={canonical} robots={robots.join(", ")} />
-	{Parser(page.content)}
-  </Layout>)
+    if (!page) return 'The slug does not exist in the CMS';
+    return (
+        <Layout>
+            <Seo
+                title={page.seo.title}
+                description={page.seo.metaDesc}
+                canonical={canonical}
+                robots="noindex, nofollow"
+            />
+            {Parser(page.content)}
+        </Layout>
+    );
 };
 
 export default PageTemplate;
