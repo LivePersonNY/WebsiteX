3d1ae46398ac4bb32913f5dd497dbaf4422b672e
Zackary Wika <48838673+zwika@users.noreply.github.com>
2026-04-14 08:09:17 -0500

    Revert "Update marketo forms to prevent gating"
    This reverts commit 96ab3511b75fcaee81ee58e1f81dd3b50cdbad97.

--- gatsby-sites/www/src/components/blocks/MktoForm.js
+++ gatsby-sites/www/src/components/blocks/MktoForm.js
@@ -1,117 +1,117 @@
-import * as React from "react";
-import { useState, useEffect } from "react";
-import { Helmet } from "react-helmet";
-import PropTypes from "prop-types";
-import Link from "gatsby-link";
-import $ from "jquery";
-import Parser from "html-react-parser";
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { Helmet } from 'react-helmet';
+import PropTypes from 'prop-types';
+import Link from 'gatsby-link';
+import $ from 'jquery';
+import Parser from 'html-react-parser';
 // import { whenPerformanceConsent } from '../../utils/marketo';
-import { whenMarketoFormsReady } from "../../utils/marketo";
+import { whenMarketoFormsReady } from '../../utils/marketo';
 
 const MktoForm = (props) => {
-  let mktoFormMobile = function (e) {
-    $(".form--sticky .mktoForm").slideToggle(300);
-    $(".span1").toggleClass("swap");
-    $(".span2").toggleClass("swap");
-  };
+    let mktoFormMobile = function (e) {
+        $('.form--sticky .mktoForm').slideToggle(300);
+        $('.span1').toggleClass('swap');
+        $('.span2').toggleClass('swap');
+    };
 
-  let formId = props.formId;
+    let formId = props.formId;
 
-  // Strictly for WP //
+    // Strictly for WP //
 
-  if (props.runFilters) {
-    const [isLoaded, setIsLoaded] = useState(false);
+    if (props.runFilters) {
+        const [isLoaded, setIsLoaded] = useState(false);
 
-    useEffect(() => {
-      // const unsubscribe = whenPerformanceConsent(() => {
-      //     setIsLoaded(true);
-      // });
-      const unsubscribe = whenMarketoFormsReady(() => {
-        setIsLoaded(true);
-      });
+        useEffect(() => {
+            // const unsubscribe = whenPerformanceConsent(() => {
+            //     setIsLoaded(true);
+            // });
+            const unsubscribe = whenMarketoFormsReady(() => {
+                setIsLoaded(true);
+            });
 
-      return unsubscribe;
-    }, []);
+            return unsubscribe;
+        }, []);
 
-    useEffect(() => {
-      if (isLoaded) {
-        if (
-          $("#mktoForm_" + formId).children().length == 0 &&
-          window.MktoForms2
-        ) {
-          window.MktoForms2.loadForm(
-            "https://info.liveperson.com",
-            "501-BLE-979",
-            formId,
-          );
-        }
-      }
-    }, [isLoaded, formId]);
-  }
+        useEffect(() => {
+            if (isLoaded) {
+                if ($('#mktoForm_' + formId).children().length == 0 && window.MktoForms2) {
+                    window.MktoForms2.loadForm('https://info.liveperson.com', '501-BLE-979', formId, function (form) {
+                        form.onValidate(function () {
+                            form.submittable(false);
+                        });
+                    });
+                }
+            }
+        }, [isLoaded, formId]);
+    }
 
-  return (
-    <div
-      data-localize={props.autoApprove && `auto-approve`}
-      autoapprove={props.autoApprove && "true"}
-      className={`${props.cssClasses} pane pane-form ${
-        props.sticky ? "form--sticky" : ""
-      } ${props.backgroundColor || "bg-rainbow"} ${
-        props.header ? "pane-with-lead-text" : ""
-      } ${props.fourFields ? "mktoForm-four-fields" : ""}`}
-      style={{ display: "none" }}
-      id={props.anchor}
-    >
-      <div className="container">
-        {props.header && !props.sticky && (
-          <div className="row">
-            <div className="col-lg-8 offset-lg-2">
-              <h2>{props.header}</h2>
-            </div>
-          </div>
-        )}
-        <div className="row">
-          <div className="col-lg-12">
-            <a className="mobileForm">
-              <span className="span1">...</span>
-              <span className="span2">
-                <svg
-                  version="1.1"
-                  viewBox="0 0 62 62"
-                  xmlns="http://www.w3.org/2000/svg"
-                  style={{ width: "40px" }}
-                >
-                  <g fill="#162036" fillRule="evenodd">
-                    <g transform="translate(1 1)" stroke="#fff">
-                      <circle cx="30" cy="30" r="30" />
-                      <g
-                        stroke="#ffffff"
-                        transform="translate(15 15)"
-                        strokeLinecap="square"
-                      >
-                        <path d="m0.51724 0.51724l29.26 29.26" />
-                        <path d="m29.483 0.51724l-29.26 29.26" />
-                      </g>
-                    </g>
-                  </g>
-                </svg>
-              </span>
-            </a>
-            <form id={`mktoForm_${formId}`} mkto={formId}></form>
-            <mkto-after mkto={formId}>
-              {props.thankyouControl || Parser(props.thankyou)}
-            </mkto-after>
-          </div>
-        </div>
-      </div>
-      {props.resourceasset && (
+    return (
         <div
-          data-resourceasset={props.resourceasset}
-          data-resourceAssetURL={props.resourceAssetURL}
-          className="display-none mkto-resource-asset"
-        ></div>
-      )}
-    </div>
-  );
+            data-localize={props.autoApprove && `auto-approve`}
+            autoapprove={props.autoApprove && 'true'}
+            className={`${props.cssClasses} pane pane-form ${
+                props.sticky ? 'form--sticky' : ''
+            } ${props.backgroundColor || 'bg-rainbow'} ${
+                props.header ? 'pane-with-lead-text' : ''
+            } ${props.fourFields ? 'mktoForm-four-fields' : ''}`}
+            style={{ display: 'none' }}
+            id={props.anchor}
+        >
+            <div className="container">
+                {props.header && !props.sticky && (
+                    <div className="row">
+                        <div className="col-lg-8 offset-lg-2">
+                            <h2>{props.header}</h2>
+                        </div>
+                    </div>
+                )}
+                <div className="row">
+                    <div className="col-lg-12">
+                        <a className="mobileForm">
+                            <span className="span1">...</span>
+                            <span className="span2">
+                                <svg
+                                    version="1.1"
+                                    viewBox="0 0 62 62"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    style={{ width: '40px' }}
+                                >
+                                    <g fill="#162036" fillRule="evenodd">
+                                        <g
+                                            transform="translate(1 1)"
+                                            stroke="#fff"
+                                        >
+                                            <circle cx="30" cy="30" r="30" />
+                                            <g
+                                                stroke="#ffffff"
+                                                transform="translate(15 15)"
+                                                strokeLinecap="square"
+                                            >
+                                                <path d="m0.51724 0.51724l29.26 29.26" />
+                                                <path d="m29.483 0.51724l-29.26 29.26" />
+                                            </g>
+                                        </g>
+                                    </g>
+                                </svg>
+                            </span>
+                        </a>
+                        <form id={`mktoForm_${formId}`} mkto={formId}></form>
+                        <mkto-after mkto={formId}>
+                            {props.thankyouControl || Parser(props.thankyou)}
+                        </mkto-after>
+                    </div>
+                </div>
+            </div>
+            {props.resourceasset && (
+                <div
+                    data-resourceasset={props.resourceasset}
+                    data-resourceAssetURL={props.resourceAssetURL}
+                    className="display-none mkto-resource-asset"
+                ></div>
+            )}
+        </div>
+    );
 };
 
 export default MktoForm;
--- gatsby-sites/www/src/components/blocks/MktoFormReport2024.js
+++ gatsby-sites/www/src/components/blocks/MktoFormReport2024.js
@@ -1,91 +1,90 @@
-import * as React from "react";
-import { useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import Link from "gatsby-link";
-import Paragraph from "../Paragraph";
-import $ from "jquery";
-import Parser from "html-react-parser";
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Link from 'gatsby-link';
+import Paragraph from '../Paragraph';
+import $ from 'jquery';
+import Parser from 'html-react-parser';
 // import { whenPerformanceConsent } from '../../utils/marketo';
-import { whenMarketoFormsReady } from "../../utils/marketo";
+import { whenMarketoFormsReady } from '../../utils/marketo';
 
 const MktoFormReport2024 = (props) => {
-  let formId = props.formId;
 
-  // Strictly for WP //
+    let formId = props.formId;
 
-  if (props.runFilters) {
-    const [isLoaded, setIsLoaded] = useState(false);
+    // Strictly for WP //
 
-    useEffect(() => {
-      // const unsubscribe = whenPerformanceConsent(() => {
-      //     setIsLoaded(true);
-      // });
-      const unsubscribe = whenMarketoFormsReady(() => {
-        setIsLoaded(true);
-      });
+    if (props.runFilters) {
+        const [isLoaded, setIsLoaded] = useState(false);
 
-      return unsubscribe;
-    }, []);
+        useEffect(() => {
+            // const unsubscribe = whenPerformanceConsent(() => {
+            //     setIsLoaded(true);
+            // });
+            const unsubscribe = whenMarketoFormsReady(() => {
+                setIsLoaded(true);
+            });
 
-    useEffect(() => {
-      if (isLoaded) {
-        if (
-          $("#mktoForm_" + formId).children().length == 0 &&
-          window.MktoForms2
-        ) {
-          window.MktoForms2.loadForm(
-            "https://info.liveperson.com",
-            "501-BLE-979",
-            formId,
-          );
-        }
-      }
-    }, [isLoaded, formId]);
-  }
+            return unsubscribe;
+        }, []);
 
-  return (
-    <>
-      <div
-        id={props.anchor}
-        className={`pane pane-form form-vertical form-vertical-2024 ${props.cssClasses}`}
-      >
-        <div className="container">
-          <div class="row bg-blue-20 align-items-center">
-            <div class="col-lg-5 offset-lg-1 order-lg-last">
-              <h2>
-                <Paragraph text={props.title} headerLevel="nothing" />
-              </h2>
-              <p></p>
-              <form id={`mktoForm_${formId}`} mkto={formId}></form>
-              <mkto-after mkto={formId}>
-                {props.thankyouControl || Parser(props.thankyou)}
-              </mkto-after>
+        useEffect(() => {
+            if (isLoaded) {
+                if ($('#mktoForm_' + formId).children().length == 0 && window.MktoForms2) {
+                    window.MktoForms2.loadForm('https://info.liveperson.com', '501-BLE-979', formId, function (form) {
+                        form.onValidate(function () {
+                            form.submittable(false);
+                        });
+                    });
+                }
+            }
+        }, [isLoaded, formId]);
+    }
+
+    return (
+        <>
+            <div
+                id={props.anchor}
+                className={`pane pane-form form-vertical form-vertical-2024 ${props.cssClasses}`}
+            >
+                <div className="container">
+                    <div class="row bg-blue-20 align-items-center">
+
+                        <div class="col-lg-5 offset-lg-1 order-lg-last">
+                            <h2>
+                                <Paragraph text={props.title} headerLevel="nothing" />
+                            </h2>
+                            <p></p>
+                            <form id={`mktoForm_${formId}`} mkto={formId}></form>
+                            <mkto-after mkto={formId}>
+                                {props.thankyouControl || Parser(props.thankyou)}
+                            </mkto-after>
+                        </div>
+                        <div class="col-lg-5 g-lg-0 order-lg-first">
+                            {(!props.imgCtl && props.imgSrc && (
+                                <img
+                                    src={props.imgSrc}
+                                    alt={props.imgAlt || ''}
+                                    width={props.imgWidth}
+                                    height={props.imgHeight}
+                                    loading="lazy"
+                                />
+                            )) ||
+                                props.imgCtl ||
+                                props.lottiePlayer}
+                        </div>
+                    </div>
+                </div>
+                {props.resourceasset && (
+                    <div
+                        data-resourceasset={props.resourceasset}
+                        data-resourceAssetURL={props.resourceAssetURL}
+                        className="display-none mkto-resource-asset"
+                    ></div>
+                )}
             </div>
-            <div class="col-lg-5 g-lg-0 order-lg-first">
-              {(!props.imgCtl && props.imgSrc && (
-                <img
-                  src={props.imgSrc}
-                  alt={props.imgAlt || ""}
-                  width={props.imgWidth}
-                  height={props.imgHeight}
-                  loading="lazy"
-                />
-              )) ||
-                props.imgCtl ||
-                props.lottiePlayer}
-            </div>
-          </div>
-        </div>
-        {props.resourceasset && (
-          <div
-            data-resourceasset={props.resourceasset}
-            data-resourceAssetURL={props.resourceAssetURL}
-            className="display-none mkto-resource-asset"
-          ></div>
-        )}
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default MktoFormReport2024;
